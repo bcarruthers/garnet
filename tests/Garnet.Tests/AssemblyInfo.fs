7b3c449c6aa994b9c9b9e2a81c5a6bacc43faa16
Ben Carruthers <ncbenc@gmail.com>
2021-04-24 19:36:23 -0400

    Bump version

--- tests/Garnet.Tests/AssemblyInfo.fs
+++ tests/Garnet.Tests/AssemblyInfo.fs
@@ -4,16 +4,16 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Garnet.Tests")>]
 [<assembly: AssemblyProductAttribute("Garnet.Tests")>]
-[<assembly: AssemblyCopyrightAttribute("Copyright © 2020 Ben Carruthers")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright © 2021 Ben Carruthers")>]
 [<assembly: AssemblyDescriptionAttribute("F# game composition library")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyVersionAttribute("0.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Garnet.Tests"
     let [<Literal>] AssemblyProduct = "Garnet.Tests"
-    let [<Literal>] AssemblyCopyright = "Copyright © 2020 Ben Carruthers"
+    let [<Literal>] AssemblyCopyright = "Copyright © 2021 Ben Carruthers"
     let [<Literal>] AssemblyDescription = "F# game composition library"
-    let [<Literal>] AssemblyVersion = "0.2.0"
-    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyVersion = "0.3.0"
+    let [<Literal>] AssemblyFileVersion = "0.3.0"
