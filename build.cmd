c3de78492f9ba5baa05ff5d0223d702fc697b0d5
Ben Carruthers <ncbenc@gmail.com>
2021-10-16 16:17:37 -0400

    Bump version and update deployment

--- build.cmd
+++ build.cmd
@@ -4,3 +4,4 @@ dotnet test --no-build --verbosity normal
 dotnet pack -c Release -o publish src\Garnet\Garnet.fsproj
 dotnet pack -c Release -o publish samples\Garnet.Numerics\Garnet.Numerics.fsproj
 dotnet pack -c Release -o publish samples\Garnet.Toolkit\Garnet.Toolkit.fsproj
+dotnet pack -c Release -o publish samples\Garnet.Processor\Garnet.Processor.fsproj
